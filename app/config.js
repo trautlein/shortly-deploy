cf40e4ec2ed55d753413f387b0a636dedfe0aef1
Hack Reactor Students <accounts@hackreactor.com>
2016-10-13 19:43:23 -0700

    Pretty much the whole sprint

--- app/config.js
+++ app/config.js
@@ -1,67 +1,11 @@
-// var path = require('path');
-// var knex = require('knex')({
-//   client: 'sqlite3',
-//   connection: {
-//     filename: path.join(__dirname, '../db/shortly.sqlite')
-//   },
-//   useNullAsDefault: true
-// });
-// var db = require('bookshelf')(knex);
-
 var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+
 mongoose.connect('mongodb://localhost/shortly');
+
 var db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'MongoDB '))
-
-mongoose.urlsSchema = new Schema({
-  url: String,
-  baseUrl: String,
-  code: String,
-  title: String,
-  visits: Number,
-  timestamps: { createdAt: 'created_at' }
+db.on('error', console.error.bind(console, 'MongoDB '));
+db.once('open', function() {
+  console.log('Connected to MongoDB and running!');
 });
-
-mongoose.usersSchema = new Schema({
-  username: String,
-  password: String,
-  timestamps: { createdAt: 'created_at' }
-});
-
-
-var Urls = mongoose.model('Urls', urlsSchema);
-var Users = mongoose.model('Users', usersSchema);
-
-
-// db.knex.schema.hasTable('urls').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('urls', function (link) {
-//       link.increments('id').primary();
-//       link.string('url', 255);
-//       link.string('baseUrl', 255);
-//       link.string('code', 100);
-//       link.string('title', 255);
-//       link.integer('visits');
-//       link.timestamps();
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
-
-// db.knex.schema.hasTable('users').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('users', function (user) {
-//       user.increments('id').primary();
-//       user.string('username', 100).unique();
-//       user.string('password', 100);
-//       user.timestamps();
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
 
 module.exports = db;
--- app/models/link.js
+++ app/models/link.js
@@ -1,19 +1,28 @@
 var db = require('../config');
 var crypto = require('crypto');
+var mongoose = require('mongoose');
 
-var Link = db.Model.extend({
-  tableName: 'urls',
-  hasTimestamps: true,
-  defaults: {
-    visits: 0
-  },
-  initialize: function() {
-    this.on('creating', function(model, attrs, options) {
-      var shasum = crypto.createHash('sha1');
-      shasum.update(model.get('url'));
-      model.set('code', shasum.digest('hex').slice(0, 5));
-    });
-  }
+var urlsSchema = mongoose.Schema ({
+  url: String,
+  link: String,
+  baseUrl: String,
+  code: String,
+  title: String,
+  visits: Number
+});
+
+var Link = mongoose.model('Link', urlsSchema);
+
+var createSha = function(url) {
+  var shasum = crypto.createHash('sha1');
+  shasum.update(url);
+  return shasum.digest('hex').slice(0, 5);
+};
+
+urlsSchema.pre('save', function(next) {
+  var code = createSha(this.url);
+  this.code = code;
+  next();
 });
 
 module.exports = Link;
